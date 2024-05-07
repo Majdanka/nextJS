45cc5a71ba9098557e1991bd49913df0362c11a9
Majdanka <majdan_k@onet.pl>
2024-05-07 12:39:48 +0200

    Chapter 9(10)

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -12,10 +12,7 @@ import { formatCurrency } from './utils';
 import { unstable_noStore as noStore } from 'next/cache';
 
 export async function fetchRevenue() {
-  // Add noStore() here to prevent the response from being cached.
-  // This is equivalent to in fetch(..., {cache: 'no-store'}).
   noStore();
-
   try {
     const data = await sql<Revenue>`SELECT * FROM revenue`;
     return data.rows;
